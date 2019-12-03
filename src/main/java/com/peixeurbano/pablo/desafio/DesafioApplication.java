ae561457a85bc304160a0f859dd93a29601cb617
Pablo Dal Maso <pdalmaso@segware.com>
2019-12-03 14:17:06 -0300

    First commit

---
+++ src/main/java/com/peixeurbano/pablo/desafio/DesafioApplication.java
@@ -0,0 +1,13 @@
+package com.peixeurbano.pablo.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
