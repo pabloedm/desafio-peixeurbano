98d6540ce06645e75a84b79c8474e2208830ccf3
Pablo Dal Maso <pdalmaso@segware.com>
2019-12-04 17:13:15 -0300

    Added integration test to assert return values

--- src/main/java/com/peixeurbano/pablo/desafio/dto/DealDTO.java
+++ src/main/java/com/peixeurbano/pablo/desafio/dto/DealDTO.java
@@ -1,5 +1,8 @@
 package com.peixeurbano.pablo.desafio.dto;
 
+import java.time.ZonedDateTime;
+
+import com.peixeurbano.pablo.desafio.enumeration.DealTypeEnum;
 import com.peixeurbano.pablo.desafio.model.Deal;
 
 public class DealDTO {
@@ -12,6 +15,38 @@ public class DealDTO {
 
     public Integer getId() {
         return deal.getId();
+    }
+
+    public String getTile() {
+        return deal.getTitle();
+    }
+
+    public String getText() {
+        return deal.getText();
+    }
+
+    public ZonedDateTime getCreateDate() {
+        return deal.getCreateDate();
+    }
+
+    public ZonedDateTime getPublishDate() {
+        return deal.getPublishDate();
+    }
+
+    public ZonedDateTime getEndDate() {
+        return deal.getEndDate();
+    }
+
+    public String getUrl() {
+        return deal.getUrl();
+    }
+
+    public Long getTotalSold() {
+        return deal.getTotalSold();
+    }
+
+    public DealTypeEnum getType() {
+        return deal.getType();
     }
 
 }
--- src/test/java/com/peixeurbano/pablo/desafio/controller/DealControllerTest.java
+++ src/test/java/com/peixeurbano/pablo/desafio/controller/DealControllerTest.java
@@ -2,7 +2,9 @@ package com.peixeurbano.pablo.desafio.controller;
 
 import static org.junit.Assert.assertEquals;
 
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -14,6 +16,25 @@ public class DealControllerTest extends BaseControllerEnviroment {
     public void listShouldReturnOK() {
         final ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + port + "/v1/deals", String.class);
         assertEquals(HttpStatus.OK, forEntity.getStatusCode());
+    }
+
+    @Test
+    public void listShouldReturnCorrectValues() throws JSONException {
+        final ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + port + "/v1/deals", String.class);
+        String expected = "[" +
+                "  {" +
+                "    \"createDate\": \"2019-12-04T19:00:00-02:00\"," +
+                "    \"endDate\": \"2019-12-14T19:00:00-02:00\"," +
+                "    \"id\": 1," +
+                "    \"publishDate\": \"2019-12-04T20:00:00-02:00\"," +
+                "    \"text\": \"Frigideira de Alumínio com Revestimento Cerâmico de 20cm, 24cm ou 28cm.\"," +
+                "    \"tile\": \"Frigideira de Alumínio\"," +
+                "    \"totalSold\": 0," +
+                "    \"type\": \"PRODUCT\"," +
+                "    \"url\": \"/frigideira+de+aluminio\"" +
+                "  }" +
+                "]";
+        JSONAssert.assertEquals(expected, forEntity.getBody(), true);
     }
 
 }
